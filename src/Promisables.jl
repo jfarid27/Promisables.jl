9d56fe0796485a314f4009d8f3ff23b50e12eb15
J. A. Farid <jrab227@gmail.com>
2017-10-11 00:16:32 -0400

    Adding tests

--- src/Promisables.jl
+++ src/Promisables.jl
@@ -1,6 +1,7 @@
 module Promisables
 
-  export Promise, Then, Pending, Rejected, Fulfilled;
+  export Promise, Then, Pending,
+  Rejected, Fulfilled, Resolve;
 
   abstract type Status end;
   type Fulfilled <: Status end;
@@ -10,41 +11,63 @@ module Promisables
   mutable struct Promise 
     status::Status
     channel::Channel{Any}
+    value::Any
+    Promise() = begin
+      c = Channel{Any}(32)
+      new(Pending(), c, nothing)
+    end
+    Promise(c::Channel{Any}) = begin
+      new(Pending(), c, nothing)
+    end
   end;
 
   function Fulfill(p::Promise, value::Any)
     if (typeof(value) == Promise)
-      Then((x) -> Fulfill(p, x), value);
+      success = (x) -> Fullfill(p, x);
+      error = (err) -> Reject(p, err);
+      Then(success, error, value);
     else
-      put!(p.channel, value);
+      p.value = value;
       p.status = Fulfilled();
+      put!(p.channel, value);
     end
   end
 
-  function Reject(p::Promise)
-    close(p.channel);
+  function Reject(p::Promise, err::Exception)
+    put!(p.channel, err);
     p.status = Rejected();
   end
 
   T1 = T where T<:Function
+  T2 = T where T<:Function
 
-  function Then(f::T1, p::Promise) 
-    Then(f, () -> (), p);
+  function Then(f::T1, p::Promise)::Promise
+    Then(f, identity, p);
   end
 
-  function Then(f::T1, err::Function, p::Promise)
+  function Then(f::T1, err::T2, p::Promise)::Promise
     newChan = Channel{Any}(32) 
-    np = Promise(Pending(), newChan);
+    np = Promise(newChan);
     @schedule begin
       value = take!(p.channel);
-      try 
-        result = f(value);
-        Fulfill(np, result);
-      catch y
-        err(y);
-        Reject(np); 
+      if (typeof(value) <: Exception)
+        value = err(value);
+        if (typeof(value) == Promise)
+          success = (x) -> Fullfill(np, x);
+          error = (nerr) -> Reject(np, nerr);
+          Then(success, error, value);
+        end
+        Reject(np, value);
+        return;
       end
+      Fulfill(np, f(value));
     end
+    return np;
+  end
+
+  function Resolve(value)::Promise
+    np = Promise();
+    @async Fulfill(np, value);
     return np;
   end
 end
---
+++ test/runtests.jl
@@ -0,0 +1,97 @@
+module Test
+
+  include("./../src/Promisables.jl");
+    
+  using Base.Test
+
+  @testset "A promise" begin
+    @testset "must resolve when fullfilled" begin
+      chan = Channel{Any}(32) 
+      p = Promisables.Promise(chan);
+      Promisables.Fulfill(p, "foo");
+      result = take!(chan);
+      @test result == "foo";
+      @test typeof(p.status) == Promisables.Fulfilled;
+      @test p.value == "foo";
+    end
+
+    @testset "must reject when rejected" begin
+      chan = Channel{Any}(32) 
+      p = Promisables.Promise(chan);
+      Promisables.Reject(p, ErrorException("Basic Error"));
+      result = take!(chan);
+      @test typeof(result) == ErrorException;
+      @test typeof(p.status) == Promisables.Rejected;
+      @test p.value == nothing;
+    end
+
+    @testset "after then is called" begin
+      @testset "when fullfilled" begin
+        @testset "must call the then's next function" begin
+          chan = Channel{Any}(32);
+          p = Promisables.Promise();
+          p1 = Promisables.Then((k) -> put!(chan, k), p);
+          Promisables.Fulfill(p, "foo");
+          result = take!(chan);
+          @test result == "foo";
+          @test typeof(p.status) == Promisables.Fulfilled;
+          @test typeof(p1.status) == Promisables.Fulfilled;
+          @test p.value == "foo";
+        end
+
+      end
+
+      @testset "when rejected" begin
+        @testset "must call the error handler if one exists with the given error" begin
+          chan = Channel{Any}(32);
+          tE = () -> error("failed");
+          errorH = (err) -> put!(chan, err);
+          p = Promisables.Promise();
+          p1 = Promisables.Then(tE, errorH, p);
+          givenError = ErrorException("Blank");
+          Promisables.Reject(p, givenError);
+          result = take!(chan);
+          @test typeof(p.status) == Promisables.Rejected;
+          @test typeof(p1.status) == Promisables.Rejected;
+          @test result == givenError;
+        end
+        @testset "if no error handler is present" begin
+          @testset "it should not continue" begin
+            chan = Channel{Any}(32);
+            tE = () -> error("failed");
+            p = Promisables.Promise();
+            p1 = Promisables.Then(tE, p);
+            givenError = ErrorException("Blank");
+            Promisables.Reject(p, givenError);
+            result = take!(p1.channel);
+            @test typeof(p.status) == Promisables.Rejected;
+            @test typeof(p1.status) == Promisables.Rejected;
+            @test result == givenError;
+          end
+        end
+        @testset "error handler" begin
+          @testset "when returning a promise" begin
+            @testset "should continue with the promise chain" begin
+              chan = Channel{Any}(32);
+              tE = () -> error("failed");
+              p = Promisables.Promise();
+              handler = (err) -> Promisables.Resolve("solved");
+              p1 = Promisables.Then(tE, handler, p);
+              givenError = ErrorException("Blank");
+              Promisables.Reject(p, givenError);
+              result = take!(p1.channel);
+              @test typeof(p.status) == Promisables.Rejected;
+              @test typeof(p1.status) == Promisables.Fulfilled;
+              @test result == "solved";
+            end
+          end
+          @testset "when throwing an error" begin
+            @testset "should not continue with the promise chain" begin
+            end
+          end
+        end
+      end
+    end
+  end
+
+end
